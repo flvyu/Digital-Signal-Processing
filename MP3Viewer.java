6844d9276b9724e480271fed661f2381e39d3dc9
Flavyoo <Flavyoo@users.noreply.github.com>
2017-07-07 22:42:18 -0400

    Update MP3Viewer.java

--- MP3Viewer.java
+++ MP3Viewer.java
@@ -1,5 +1,5 @@
 /**********************************************************************************
- *  Author        Flavio Andrade
+ * 
  *  Compilation:  javac -cp .:stdplayer.jar MP3Viewer.java
  *  Execution:    java  -cp .:stdplayer.jar MP3Viewer audio
  *  Dependencies: Wave.java
@@ -17,24 +17,24 @@ public class MP3Viewer {
 	public static void main(String[] args) {
 		double yscale = 300;
 		String filename = args[0];
-        StdPlayer.open(filename);
+                StdPlayer.open(filename);
 		StdDraw.setCanvasSize(576, 512);
 		StdDraw.enableDoubleBuffering();
 		StdDraw.setPenColor(StdDraw.BLUE);
 		StdDraw.setXscale(-30, 1182);
 		StdDraw.setYscale(-yscale, yscale);
 		int i = 1;
-        while (!StdPlayer.isEmpty()) {
-            Wave w = new Wave(StdPlayer.getLeftChannel(), StdPlayer.getRightChannel());
-			w.play();
-			// Every other wave.
-			if (i % 2 == 0) {
-				w.draw(i / 2);
-			}
-			StdDraw.pause(10);
-			i++;
-        }
-        StdPlayer.close();
-        System.exit(0);
-        }
-	}
+                while (!StdPlayer.isEmpty()) {
+		    Wave w = new Wave(StdPlayer.getLeftChannel(), StdPlayer.getRightChannel());
+				w.play();
+				// Every other wave.
+				if (i % 2 == 0) {
+					w.draw(i / 2);
+				}
+				StdDraw.pause(10);
+				i++;
+		}
+		StdPlayer.close();
+		System.exit(0);
+    }
+}
